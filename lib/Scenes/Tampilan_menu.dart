3b443abdf8a13f6557ac4cf61a9f8c285a77e81c
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2023-03-12 23:03:08 +0700

    Update Tampilan_menu.dart
    - menambahkan opsi tambah dari galeri
    - mengirim dan menerima API flask
    - mengganti gambar ambil gambar

--- lib/Scenes/Tampilan_menu.dart
+++ lib/Scenes/Tampilan_menu.dart
@@ -3,7 +3,7 @@
 import 'dart:io';
 
 import 'package:face_shape/Scenes/Tampilan_ambil_gambar.dart';
-import 'package:face_shape/Scenes/Tampilan_awal.dart';
+import 'package:face_shape/Scenes/Tampilan_hasil.dart';
 import 'package:face_shape/Scenes/Tampilan_panduan.dart';
 import 'package:face_shape/widgets/custom_button.dart';
 import 'package:face_shape/widgets/custom_media.dart';
@@ -11,6 +11,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart'
+    show rootBundle, ByteData, PlatformException;
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
@@ -22,6 +28,33 @@ class MainMenu extends StatefulWidget {
 class _MainMenuState extends State<MainMenu> {
   // late File _image;
   late File _imageFile;
+
+  Future<void> copyImageToAssets(String imagePath, String assetPath) async {
+    try {
+      // Buka file gambar dari path yang diberikan
+      final File imageFile = File(imagePath);
+      print("cell");
+
+      // Baca file gambar sebagai byte data
+      final ByteData imageData = await imageFile
+          .readAsBytes()
+          .then((data) => ByteData.view(data.buffer));
+
+      print("cell2");
+
+      // Salin file gambar ke dalam folder aset
+      await writeToFile(imageData, assetPath);
+
+      //   print("File berhasil disalin ke folder aset");
+    } on PlatformException catch (e) {
+      print("Gagal menyalin file ke folder aset: $e");
+    }
+  }
+
+  Future<void> writeToFile(ByteData data, String path) {
+    return new File(path).writeAsBytes(
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,8 +107,29 @@ class _MainMenuState extends State<MainMenu> {
               final pickedFile =
                   await picker.pickImage(source: ImageSource.gallery);
               if (pickedFile != null) {
-                // Lakukan sesuatu pada gambar yang dipilih
-                _imageFile = File(pickedFile.path);
+                final url = Uri.parse('http://192.168.1.113:8000/upload');
+
+                final request = http.MultipartRequest('POST', url)
+                  ..files.add(await http.MultipartFile.fromPath(
+                      'file', pickedFile.path));
+                final client = http.Client();
+                final response = await client
+                    .send(request)
+                    .then((response) => http.Response.fromStream(response));
+
+                if (response.statusCode == 200) {
+                  final jsonResponse = json.decode(response.body);
+                  print("response: " + jsonResponse.toString());
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.rightToLeftWithFade,
+                      child: ReportScreen(),
+                    ),
+                  );
+                } else {
+                  print('Request failed with status: ${response.statusCode}.');
+                }
               }
             },
             text: "Pilih Gambar",
@@ -89,13 +143,15 @@ class _MainMenuState extends State<MainMenu> {
               Navigator.push(
                 context,
                 PageTransition(
-                    type: PageTransitionType.rightToLeftWithFade,
+                    type: PageTransitionType.size,
+                    alignment: Alignment.center,
+                    duration: Duration(seconds: 1),
                     child: CameraScreen()),
                 // ),
               );
             },
-            text: "Pilih Gambar",
-            imageAsset: 'Assets/Images/gallery.jpg',
+            text: "Ambil Gambar",
+            imageAsset: 'Assets/Images/camera.jpg',
           ),
           SizedBox(
             height: 35,
