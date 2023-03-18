6b010ff85159a3ea8e44f194f215b424dab805d1
Herlambang Kurniawan <71071542+inoyamanaka@users.noreply.github.com>
2023-03-18 20:15:13 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,6 +4,12 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from pyngrok import ngrok
+from tensorflow import keras
+from deepface import DeepFace
+import keras.utils as image
+
+model = keras.models.load_model('fc_model.h5')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
 app = Flask(__name__)
 
@@ -14,6 +20,7 @@ app = Flask(__name__, template_folder='./templates')
 
 UPLOAD_FOLDER = './upload'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+list_class = ['Diamond','Oblong','Oval','Round','Square','Triangle']
 
 # INITIALIZING OBJECTS
 mp_drawing = mp.solutions.drawing_utils
@@ -43,9 +50,21 @@ def upload_file():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        detect_landmark(filepath)
-        return jsonify({'message': 'File successfully uploaded'})
+
+        # Detect Face
+        try:
+            img = cv2.imread(filepath)
+            DeepFace.detectFace(img)
+            file.save(filepath)
+            print(filepath)
+            preprocessing(filepath)
+
+
+
+            return jsonify({'message': 'File successfully uploaded'})
+        except:
+            path = "empty_image.png"
+            return jsonify({'message': 'File failed to uploaded'})
 
     return jsonify({'message': 'Allowed file types are png, jpg, jpeg, gif'}), 400
 
@@ -53,14 +72,39 @@ def upload_file():
 @app.route('/get_images')
 def get_images():
     urls = []
-    for i in range(1, 4):
+    for i in range(1, 5):
         filename = f'result_upload{i}.jpg'
         url = f'{public_url}/static/{filename}'
         urls.append(url)
-    return jsonify({'urls': urls})
 
-def detect_landmark(filepath):
-    print("jalan : ", filepath)
+        bentuk, persentase = prediction()
+        print("apakah ini bentuk wajah : ",bentuk[0])
+        print("apakah ini persentase : ", persentase)
+
+    return jsonify({'urls': urls, 'bentuk wajah':bentuk[0], 'persen':persentase})
+
+
+def preprocessing(filepath):
+    face_cropping(filepath)
+    detect_landmark()
+
+def face_cropping(filepath):
+    offset = 50
+    img = cv2.imread(filepath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x, y, w, h) in faces:
+        y_offset = 20
+        # faces = img[y:y + h + 30, x:x + w + 30]
+        faces = img[max(0, y - offset + y_offset):min(y + h + offset + y_offset, img.shape[0]),
+                max(0, x - offset):min(x + w + offset, img.shape[1])]
+        # cv2.imwrite("result_image_1.jpg", faces)
+        cv2.imwrite("./static/result_upload1.jpg", faces)
+
+def detect_landmark():
+    filepath = "./static/result_upload1.jpg"
+
     # READ IMAGE FILE
     image1 = cv2.imread(filepath)
     image1 = cv2.resize(image1, (220, 280), interpolation=cv2.INTER_AREA)
@@ -85,7 +129,7 @@ def detect_landmark(filepath):
 
         # Convert back to the BGR color space
         image2 = cv2.cvtColor(image2, cv2.COLOR_RGB2BGR)
-        cv2.imwrite('./static/result_upload1.jpg', image2)
+        cv2.imwrite('./static/result_upload2.jpg', image2)
 
 
         # Draw the face mesh annotations on the image.
@@ -100,7 +144,7 @@ def detect_landmark(filepath):
 
         # imagen = imageb - image
         # imagen = cv2.subtract(image, imageb)
-        cv2.imwrite('./static/result_upload2.jpg', image2)
+        cv2.imwrite('./static/result_upload3.jpg', image2)
         subtracted_img = np.zeros(image1.shape, np.uint8)
 
         # Lakukan perhitungan pengurangan pixel secara manual
@@ -108,10 +152,29 @@ def detect_landmark(filepath):
             for j in range(image1.shape[1]):
                 subtracted_img[i, j] = abs(int(image1[i, j][0]) - int(image2[i, j][0]))
 
-        cv2.imwrite('./static/result_upload3.jpg', subtracted_img)
+        cv2.imwrite('./static/result_upload4.jpg', subtracted_img)
+
+def prediction():
+    img_width, img_height = 200, 200
+    img = image.load_img("./static/result_upload4.jpg", target_size=(img_width, img_height))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    pred = model.predict(img)
+
+    # menghitung softmax
+    softmax_pred = np.exp(pred) / np.sum(np.exp(pred), axis=1, keepdims=True)
+    max_value = int(round(np.max(softmax_pred * 100)))
+
+    pred = np.argmax(pred, axis=1)
+
+    # Map the label
+    pred = [list_class[k] for k in pred]
+
+    return pred, max_value
 
 if __name__ == '__main__':
     # app.run(port=8000, host='192.168.1.113')
+    # ngrok.set_config(http_tunnel_port="8080")
     public_url = ngrok.connect(5000).public_url
     print(f' * Running on {public_url}')
 
